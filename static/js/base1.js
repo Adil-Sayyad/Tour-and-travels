7b1aa08c05f44ec781453e4125b894d8d711cca3
Adil-Sayyad <adilsayyadr@gmail.com>
2024-03-02 02:27:36 -0800

    tours and travels project

---
+++ static/js/base1.js
@@ -0,0 +1,12 @@
+document.getElementById('start').addEventListener('change', function () {
+    document.getElementById('myForm').submit();
+});
+document.getElementById('start1').addEventListener('change', function () {
+    document.getElementById('myForm').submit();
+});
+// document.getElementById('start3').addEventListener('change', function () {
+//     document.getElementById('myForm1').submit();
+// });
+// document.getElementById('start4').addEventListener('change', function () {
+//     document.getElementById('myForm1').submit();
+// });
